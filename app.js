9a01367da925a2fe8f29d7c29dc202ab5ee3e230
Ezequiel Calderara <ezechico@gmail.com>
2014-02-26 18:03:54 -0300

    Me gusta mas express

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-var connect = require('connect');
-connect.createServer(
-    connect.static(__dirname)
-).listen(3000);
+var express = require('express');
+var app = express();
+//var path = require('path');
+
+app.use(express.static(__dirname)); // Current directory is root
+//app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of current is root
+app.get("/api/search::term", function(req)
+{
+	console.log(req.params);
+});
+app.listen(3000);
+console.log('Listening on port 3000');
